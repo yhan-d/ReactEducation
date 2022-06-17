7c04996530fa0cdae95198e725f9bd1b76836cac
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-06-17 14:08:25 +0300

    Add project files.

---
+++ 2-Components/src/Components/Test.js
@@ -0,0 +1,13 @@
+// function base (fonksiyon + State)
+// class base (fonksiyon + State)
+
+export default function Test(){
+    return (
+            <div>
+                <h1>Test Component</h1>
+                <p>Merhaba</p>
+            </div>
+    )
+        
+    
+}
