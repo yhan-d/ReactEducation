17ecde50459759874c1789e8099ce6e272c7a87a
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-06-28 14:59:35 +0300

    Api ye bağlandım ve kitapları çektim

--- 4-State/GlobalState/Book/src/components/Books.js
+++ 4-State/GlobalState/Book/src/components/Books.js
@@ -5,7 +5,43 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Books() {
   const [state, dispatch] = useContext(BooksellerContext);
+  function getBooks() {
+    axios
+    .get("http://book-store-api-test.herokuapp.com/books")
+    .then((response) => {
+      dispatch({ type:"FETCH_BOOKS", payload: response.data});
+    });
+  }
+  useEffect( () => {
+    getBooks();},[]);
+    console.log(state);
+    const bList = state.books.map((b) => {
+      return(
+        <tr key={b.id}>
+          <td>{b.id}</td>
+          <td><img src= {b.imgUrl} style={{ height:"350", width:"150"}}/></td>
+          <td>{b.name}</td>
+          <td>{b.author}</td>
+          <td>{b.about}</td>
+        </tr>
+      )
+    })
+
+
   return (
-    <div>Books</div>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Author</th>
+          <th>About</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bList}
+      </tbody>
+    </table>
   )
 }
